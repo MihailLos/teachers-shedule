0e1f6ccb1d5b47a7ab989f4e98bb70edfee3f61a
mihaillos <enderclub12@gmail.com>
2023-02-09 17:46:55 +0700

    maked shedule table interface

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import "./bootstrap.css";
 
 function App() {
+  let weekDayList = [
+    { name: "Понедельник" },
+    { name: "Вторник" },
+    { name: "Среда" },
+    { name: "Четверг" },
+    { name: "Пятница" },
+    { name: "Суббота" },
+  ];
+
+  let weekTypeList = [{ name: "Чет" }, { name: "Нечет" }];
+
+  let coupleList = [
+    { name: "8.00-9.35" },
+    { name: "9.45-11.20" },
+    { name: "11.45-13.20" },
+    { name: "13.30-15.05" },
+    { name: "15.30-17.05" },
+    { name: "17.15-18.50" },
+    { name: "19.00-20.35" },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Ф.И.О. преподавателя</h1>
+      <body>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Дни недели</th>
+              <th>Неделя</th>
+              {coupleList.map((couple) => {
+                return <th>{couple.name}</th>;
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {weekDayList.map((weekDay) => {
+              return weekTypeList.map((weekType) => {
+                return (
+                  <tr>
+                    <td>{weekDay.name}</td>
+                    <td>{weekType.name}</td>
+                    {coupleList.map((couple) => {
+                      return (
+                        <td>
+                          <label>
+                            <input
+                              type={"checkbox"}
+                              className="form-check-input"
+                            />
+                            &nbsp; Не выставлять
+                          </label>
+                        </td>
+                      );
+                    })}
+                  </tr>
+                );
+              });
+            })}
+          </tbody>
+        </table>
+      </body>
     </div>
   );
 }
