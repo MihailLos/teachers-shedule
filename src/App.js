566e6fa8021053c93c936c19cc2ff764bf8393d4
mihaillos <enderclub12@gmail.com>
2023-03-09 16:52:39 +0700

    Add post request on checkbox click

--- src/App.js
+++ src/App.js
@@ -31,12 +31,10 @@ function SemesterDropDown() {
     <div>
       <select
         onChange={onChangeDropdown}
-        class="form-select"
+        className="form-select"
         aria-label="Default select example"
       >
-        <option value={""} selected>
-          Выберите семестр
-        </option>
+        <option value={""}>Выберите семестр</option>
         {semesterList.map((semester) => {
           return (
             <option key={semester.Id} value={semester.Id}>
@@ -45,14 +43,16 @@ function SemesterDropDown() {
           );
         })}
       </select>
-      {showTeachers ? <TeacherDropDown semId={chosenSemester.Id} /> : null}
+      {showTeachers ? (
+        <TeacherDropDown chosenSemester={chosenSemester} />
+      ) : null}
     </div>
   );
 }
 
 function TeacherDropDown(props) {
   let accessToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYWNjZXNzIiwiaXAiOiIxNzYuMTk2LjE2LjE4MCIsInVzZXJJZCI6MTcwMzAsImlhdCI6MTY3NzE0NDYzMiwiZXhwIjoxNjc3MjMxMDMyfQ.7ktfo44A4tBJJc1Upl4z_n9lnm93uEeaZ3CFLZXXOpg";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYWNjZXNzIiwiaXAiOiIxNzYuMTk2LjE2LjE4MCIsInVzZXJJZCI6MTcwMzAsImlhdCI6MTY3ODI4ODAxMywiZXhwIjoxNjc4Mzc0NDEzfQ.YxQXiCSA-BBdhIjErwjIlvM3H5VOBBdZj6VU1y9Yz6A";
   let [teachersList, setList] = useState([]);
 
   let [chosenTeacher, setChosenTeacher] = useState(null);
@@ -61,7 +61,6 @@ function TeacherDropDown(props) {
 
   const onChangeDropdown = (event) => {
     event.target.value ? setShowTable(true) : setShowTable(false);
-    console.log(teachersList);
     setChosenTeacher(
       teachersList.find(
         (element) => element.prepId === Number(event.target.value)
@@ -71,7 +70,7 @@ function TeacherDropDown(props) {
 
   useEffect(() => {
     fetch(
-      `https://api-next.kemsu.ru/api/schedule/integration/teacherList?semesterId=${props.semId}`,
+      `https://api-next.kemsu.ru/api/schedule/integration/teacherList?semesterId=${props.chosenSemester.Id}`,
       {
         headers: {
           "X-Access-Token": accessToken,
@@ -88,12 +87,10 @@ function TeacherDropDown(props) {
     <div>
       <select
         onChange={onChangeDropdown}
-        class="form-select"
+        className="form-select"
         aria-label="Default select example"
       >
-        <option value={""} selected>
-          Выберите преподавателя
-        </option>
+        <option value={""}>Выберите преподавателя</option>
         {teachersList.map((teacher) => {
           return (
             <option key={teacher.prepId} value={teacher.prepId}>
@@ -102,17 +99,47 @@ function TeacherDropDown(props) {
           );
         })}
       </select>
-      {showTable ? <SheduleTable teacherFio={chosenTeacher.fio} /> : null}
+      {showTable ? (
+        <SheduleTable
+          chosenTeacher={chosenTeacher}
+          chosenSemester={props.chosenSemester}
+        />
+      ) : null}
     </div>
   );
 }
 
 function SheduleTable(props) {
+  let accessToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYWNjZXNzIiwiaXAiOiIxNzYuMTk2LjE2LjE4MCIsInVzZXJJZCI6MTcwMzAsImlhdCI6MTY3ODI4ODAxMywiZXhwIjoxNjc4Mzc0NDEzfQ.YxQXiCSA-BBdhIjErwjIlvM3H5VOBBdZj6VU1y9Yz6A";
+
   let [weekDayList, setWeekDayList] = useState([]);
 
   let [coupleList, setCoupleList] = useState([]);
 
   let weekTypeList = [{ name: "Чет" }, { name: "Нечет" }];
+
+  function updateFlags(coupleId, weekDayId, restrictionFlag) {
+    fetch(
+      "https://api-next.kemsu.ru/api/schedule/integration/updatePrepRestriction",
+      {
+        method: "POST",
+        headers: {
+          "X-Access-Token": accessToken,
+          "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify({
+          prepId: props.chosenTeacher.prepId,
+          semesterId: props.chosenSemester.Id,
+          coupleId: coupleId,
+          weekDayId: weekDayId,
+          restrictionFlag: +restrictionFlag,
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(JSON.stringify(response)));
+  }
 
   useEffect(() => {
     fetch("https://api-next.kemsu.ru/api/schedule/integration/weekDayList")
@@ -129,14 +156,14 @@ function SheduleTable(props) {
 
   return (
     <div>
-      <h1>{props.teacherFio}</h1>
+      <h1>{props.chosenTeacher.fio}</h1>
       <table className="table table-striped">
         <thead>
           <tr>
             <th>Дни недели</th>
             <th>Неделя</th>
             {coupleList.map((couple) => {
-              return <th>{couple.Description}</th>;
+              return <th key={couple.Id}>{couple.Description}</th>;
             })}
           </tr>
         </thead>
@@ -145,7 +172,11 @@ function SheduleTable(props) {
             return weekTypeList.map((weekType, idx) => {
               return (
                 <tr>
-                  {idx % 2 == 0 ? <td rowSpan={2}>{weekDay.DayName}</td> : null}
+                  {idx % 2 === 0 ? (
+                    <td rowSpan={2} key={weekDay.Id}>
+                      {weekDay.DayName}
+                    </td>
+                  ) : null}
                   <td>{weekType.name}</td>
                   {coupleList.map((couple) => {
                     return (
@@ -153,10 +184,15 @@ function SheduleTable(props) {
                         <label>
                           <input
                             type={"checkbox"}
-                            // onChange={(event) => {
-                            //   onChange_2(weekId, coo);
-                            // }}
                             className="form-check-input"
+                            key={Math.random()}
+                            onChange={(event) => {
+                              updateFlags(
+                                couple.Id,
+                                weekDay.Id,
+                                Boolean(event.target.value)
+                              );
+                            }}
                           />
                           &nbsp; Не выставлять
                         </label>
