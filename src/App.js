8ab1568ab7256a7aea1eb4d53ea206f5ae76cbcc
mihaillos <enderclub12@gmail.com>
2023-04-04 18:54:39 +0700

    Fetching limitation through API

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ function App() {
   return <SemesterDropDown />;
 }
 
+let accessToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYWNjZXNzIiwiaXAiOiI4Mi4xNzkuMS45IiwidXNlcklkIjoxNzAzMCwiaWF0IjoxNjgwNTkwMzMyLCJleHAiOjE2ODA2NzY3MzJ9.ibU_agWDxcl8vWxKNY5jZdsG1rk3S7IqBuKoytWRRq0";
+
 function SemesterDropDown() {
   let [semesterList, setList] = useState([]);
 
-  let [chosenSemester, setChosenSemester] = useState(null);
+  let [loading, setLoading] = useState(true);
 
-  const [showTeachers, setShowTeachers] = useState(false);
+  let [chosenSemester, setChosenSemester] = useState("");
 
   const onChangeDropdown = (event) => {
-    event.target.value ? setShowTeachers(true) : setShowTeachers(false);
     setChosenSemester(
       semesterList.find((element) => element.Id === Number(event.target.value))
     );
@@ -24,8 +26,19 @@ function SemesterDropDown() {
       .then((res) => res.json())
       .then((res) => {
         setList(res.result);
+        setLoading(false);
       });
   }, []);
+
+  if (loading) {
+    return (
+      <div class="d-flex justify-content-center">
+        <div class="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -33,8 +46,9 @@ function SemesterDropDown() {
         onChange={onChangeDropdown}
         className="form-select"
         aria-label="Default select example"
+        value={chosenSemester ? chosenSemester.Id : ""}
       >
-        <option value={""}>Выберите семестр</option>
+        <option value="">Выберите семестр</option>
         {semesterList.map((semester) => {
           return (
             <option key={semester.Id} value={semester.Id}>
@@ -43,21 +57,24 @@ function SemesterDropDown() {
           );
         })}
       </select>
-      {showTeachers ? (
-        <TeacherDropDown chosenSemester={chosenSemester} />
-      ) : null}
+      {chosenSemester && (
+        <TeacherDropDown
+          chosenSemester={chosenSemester}
+          key={chosenSemester.Id}
+        />
+      )}
     </div>
   );
 }
 
 function TeacherDropDown(props) {
-  let accessToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYWNjZXNzIiwiaXAiOiIxNzYuMTk2LjE2LjE4MCIsInVzZXJJZCI6MTcwMzAsImlhdCI6MTY3ODI4ODAxMywiZXhwIjoxNjc4Mzc0NDEzfQ.YxQXiCSA-BBdhIjErwjIlvM3H5VOBBdZj6VU1y9Yz6A";
   let [teachersList, setList] = useState([]);
 
-  let [chosenTeacher, setChosenTeacher] = useState(null);
+  let [chosenTeacher, setChosenTeacher] = useState("");
 
   const [showTable, setShowTable] = useState(false);
+
+  let [loading, setLoading] = useState(true);
 
   const onChangeDropdown = (event) => {
     event.target.value ? setShowTable(true) : setShowTable(false);
@@ -80,8 +97,19 @@ function TeacherDropDown(props) {
       .then((res) => res.json())
       .then((res) => {
         setList(res.teacherList);
+        setLoading(false);
       });
-  }, [props.semId]);
+  }, [props.chosenSemester]);
+
+  if (loading) {
+    return (
+      <div class="d-flex justify-content-center">
+        <div class="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div>
@@ -89,6 +117,7 @@ function TeacherDropDown(props) {
         onChange={onChangeDropdown}
         className="form-select"
         aria-label="Default select example"
+        defaultValue={chosenTeacher}
       >
         <option value={""}>Выберите преподавателя</option>
         {teachersList.map((teacher) => {
@@ -110,16 +139,22 @@ function TeacherDropDown(props) {
 }
 
 function SheduleTable(props) {
-  let accessToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiYWNjZXNzIiwiaXAiOiIxNzYuMTk2LjE2LjE4MCIsInVzZXJJZCI6MTcwMzAsImlhdCI6MTY3ODI4ODAxMywiZXhwIjoxNjc4Mzc0NDEzfQ.YxQXiCSA-BBdhIjErwjIlvM3H5VOBBdZj6VU1y9Yz6A";
-
   let [weekDayList, setWeekDayList] = useState([]);
 
   let [coupleList, setCoupleList] = useState([]);
 
-  let weekTypeList = [{ name: "Чет" }, { name: "Нечет" }];
+  let weekTypesList = [
+    { name: "Не выбрано", num: null },
+    { name: "Каждую", num: 1 },
+    { name: "Чет", num: 2 },
+    { name: "Нечет", num: 3 },
+  ];
 
-  function updateFlags(coupleId, weekDayId, restrictionFlag) {
+  let [fetchedWeekTypeList, setFetchedWeekTypeList] = useState([]);
+
+  let [loading, setLoading] = useState(true);
+
+  function updateFlags(coupleId, weekDayId, periodTypeId) {
     fetch(
       "https://api-next.kemsu.ru/api/schedule/integration/updatePrepRestriction",
       {
@@ -133,7 +168,7 @@ function SheduleTable(props) {
           semesterId: props.chosenSemester.Id,
           coupleId: coupleId,
           weekDayId: weekDayId,
-          restrictionFlag: +restrictionFlag,
+          periodTypeId: +periodTypeId,
         }),
       }
     )
@@ -154,6 +189,33 @@ function SheduleTable(props) {
       });
   }, []);
 
+  useEffect(() => {
+    fetch(
+      `https://api-next.kemsu.ru/api/schedule/integration/prepRestriction?prepId=${props.chosenTeacher.prepId}&semesterId=${props.chosenSemester.Id}`,
+      {
+        method: "GET",
+        headers: {
+          "X-Access-Token": accessToken,
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setFetchedWeekTypeList(res.result);
+        setLoading(false);
+      });
+  }, [props.chosenTeacher.prepId]);
+
+  if (loading) {
+    return (
+      <div class="d-flex justify-content-center">
+        <div class="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>{props.chosenTeacher.fio}</h1>
@@ -161,47 +223,49 @@ function SheduleTable(props) {
         <thead>
           <tr>
             <th>Дни недели</th>
-            <th>Неделя</th>
             {coupleList.map((couple) => {
               return <th key={couple.Id}>{couple.Description}</th>;
             })}
           </tr>
         </thead>
         <tbody>
-          {weekDayList.map((weekDay) => {
-            return weekTypeList.map((weekType, idx) => {
-              return (
-                <tr>
-                  {idx % 2 === 0 ? (
-                    <td rowSpan={2} key={weekDay.Id}>
-                      {weekDay.DayName}
+          {weekDayList.map((weekDay, idx) => {
+            return (
+              <tr key={Math.random()}>
+                <td>{weekDay.DayName}</td>
+                {coupleList.map((couple, idx2) => {
+                  var item = fetchedWeekTypeList[idx][idx2];
+                  var result = weekTypesList.find((obj) => {
+                    return obj.num == item;
+                  });
+                  return (
+                    <td>
+                      <select
+                        value={result.num}
+                        key={Math.random()}
+                        onChange={(event) => {
+                          updateFlags(
+                            couple.Id,
+                            weekDay.Id,
+                            event.target.value
+                          );
+                        }}
+                        className="form-select"
+                        aria-label="Default select example"
+                      >
+                        {weekTypesList.map((weekType) => {
+                          return (
+                            <option key={Math.random()} value={weekType.num}>
+                              {weekType.name}
+                            </option>
+                          );
+                        })}
+                      </select>
                     </td>
-                  ) : null}
-                  <td>{weekType.name}</td>
-                  {coupleList.map((couple) => {
-                    return (
-                      <td>
-                        <label>
-                          <input
-                            type={"checkbox"}
-                            className="form-check-input"
-                            key={Math.random()}
-                            onChange={(event) => {
-                              updateFlags(
-                                couple.Id,
-                                weekDay.Id,
-                                Boolean(event.target.value)
-                              );
-                            }}
-                          />
-                          &nbsp; Не выставлять
-                        </label>
-                      </td>
-                    );
-                  })}
-                </tr>
-              );
-            });
+                  );
+                })}
+              </tr>
+            );
           })}
         </tbody>
       </table>
